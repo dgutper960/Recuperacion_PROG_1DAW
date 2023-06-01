50dafdda2c9f3e94859e6e23f07f2d8190bba17f
dgutper960 <dgutper960@outlook.es>
2023-06-01 08:46:43 +0200

    Initial commit

---
+++ src/Tema_2/Actividad_2_2/Hola_Mundo.java
@@ -0,0 +1,15 @@
+package Tema_2.Actividad_2_2;
+/** Previamente, hemos creado el proyecto que abarca toda la recuperación
+ * para la asignatura: Dentro del proyecto vamos a crear paquetes
+ * para cada tema y en cada tema, paquetes para cada actividad.
+ * Dentro del paquete correspondiente, vamos a crear la clase con
+ * el método principal (el método main) el cual nos va a permitir
+ * ejecutar el programa */
+public class Hola_Mundo { // Nombre de la clase
+    public static void main(String[] args) { // Método main
+        /** Vamos a recurrir al método printnl de la clase System.out
+         * el método println no va a imprimir un literal por pantalla
+         * (se puede llamar a variables y demás), printnl añade un salto de línea */
+        System.out.println("Hola Mundo"); // Código para él Hola Mundo
+    }
+}
---
+++ src/Tema_2/Actividad_2_3/par_Impar.java
@@ -0,0 +1,4 @@
+package Tema_2.Actividad_2_3;
+
+public class par_Impar {
+}
