0ceefcf2c21b8382a04c90141f8a408784138df0
dgutper960 <dgutper960@outlook.es>
2023-06-01 11:46:17 +0200

    Se inicia prueba para Examen 2

---
+++ src/Tema_2/PruebaExamen_1/Ejercicio_1.java
@@ -0,0 +1,35 @@
+package Tema_2.PruebaExamen_1;
+
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+
+        /** Ejercicio 1. [ 2 puntos]
+         Muestra por la consola el mayor de los tres números dados por 3 variables. */
+        Integer a = 3, b = 5, c = 7; // Declaramos los números
+        Integer mayor; // Declaramos variable para almacenar el mayor
+        /** Para este ejercicio podemos usar el método max de la clase Math
+         *  inicializaremos la variable mayor con lo que nos devuelva el método max */
+        mayor = Math.max(Math.max(a, b), c); // Observamos la sintaxis
+        /** 1º Extraemos el mayor entre a y b
+         *  2º Comparamos lo que extrae la primera consulta con la variable c */
+
+        /** Sin usar la clase Math podríamos usar una estructura condicional */
+
+                if (a > b) {
+                    if (a > c) {
+                        mayor = a;
+                    } else {
+                        mayor = c;
+                    }
+                } else {
+                    if (b > c) {
+                        mayor = b;
+                    } else {
+                        mayor = c;
+                    }
+                }
+
+                System.out.println("El mayor de los tres números es: " + mayor);
+
+    }
+}
---
+++ src/Tema_2/PruebaExamen_1/Ejercicio_2.java
@@ -0,0 +1,37 @@
+package Tema_2.PruebaExamen_1;
+
+/** Calcula el primer y último número primo entre dos números dados (inclusives).
+ Ej: a = 20, b = 30 → primerPrimo = 23, ultimoPrimo = 29
+ Ej: a = 17, b = 29 → primerPrimo = 17, ultimoPrimo = 29 */
+
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        /** Inicializamos a=20 y b=30 */
+                int a = 20, b = 30;
+        /** Declaramos variables para 1er y último primo */
+                int primerPrimo = 0, ultimoPrimo = 0;
+
+                /** Creamos un bucle for */
+                for (int i = a; i <= b; i++) {  // Se inicializa i al valor de a y mientras sea < b, incremento +1
+                    boolean esPrimo = true;
+
+                    for (int j = 2; j <= Math.sqrt(i); j++) {
+                        if (i % j == 0) {
+                            esPrimo = false;
+                            break;
+                        }
+                    }
+
+                    if (esPrimo) {
+                        if (primerPrimo == 0) {
+                            primerPrimo = i;
+                        }
+                        ultimoPrimo = i;
+                    }
+                }
+
+                System.out.println("El primer número primo es: " + primerPrimo);
+                System.out.println("El último número primo es: " + ultimoPrimo);
+
+    }
+}
