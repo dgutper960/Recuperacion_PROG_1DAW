6fe68a7af7c7a7c366f3a8e5f02c56e72df72f87
dgutper960 <dgutper960@outlook.es>
2023-06-02 11:27:37 +0200

    Inicio de la UD_3

---
+++ src/Tema_3/Actividad_3_2/Main.java
@@ -0,0 +1,35 @@
+package Tema_3.Actividad_3_2;
+/** Creamos una Clase Main para instanciar tantas nuevas personas de la Clase Persona como sea necesario*/
+
+public class Main {
+    public static void main(String[] args) {
+        /** Vamos a instanciar diferentes personas de diferentes formas y con sus respectivos atributos */
+
+        Persona persona1 = new Persona();
+        Persona persona2 = new Persona();
+
+        persona1.setNombre("David");
+        persona1.setPrimApellido("Gutiérrez");
+        persona1.setSegApellido("Pérez");
+        persona1.setEdad(38);
+        System.out.println("La persona1 se llama "+ persona1.getNombre()+
+                "\nSus aperllidos son "+persona1.getPrimApellido()+ persona1.getSegApellido()+
+                "\ny tiene "+persona1.getEdad()+ " muchas gracias!! ciao, ciao!");
+
+        System.out.println("Persona2");
+        System.out.println("Nombre: " + persona2.getNombre());
+        System.out.println("Edad: " + persona2.getEdad());
+
+        Persona persona3 = new Persona("Antonio", "González");
+        persona3.setSegApellido("Flores");
+        System.out.println("La persona3 es conocida como "+persona3.getNombre()+" "+persona3.getSegApellido()+
+        "\ny cuenta la leyenda que era el único músico de entre sus herman@s");
+
+        /** En este caso accederemos a un método creado enn la Clase Persona*/
+        Persona persona4 = new Persona();
+        persona4.setEdad(15);
+        System.out.println("De esta persona solo sabemos que tiene "+persona4.getEdad()+" años");
+        System.out.println("Nos preguntamos si será el músico legendario "+persona4.esMusico);
+
+    }
+}
---
+++ src/Tema_3/Actividad_3_2/Persona.java
@@ -0,0 +1,71 @@
+package Tema_3.Actividad_3_2;
+/** Creamos esta Clase llamada Persona, que será de visibilidad Pública
+ * le vamos a dar una serie de atributos: nombre, primApellido, segApellido y edad */
+
+public class Persona {
+    public String esMusico; /** para poder acceder al método desde la clase Main */
+
+    private String nombre;
+
+    private String primApellido;
+    private String segApellido;
+    private int edad;
+
+    Persona () {
+        nombre = "Anónimo";
+    }
+
+    /** Creamos los getters y los setters para los diferentes atributos
+     * --> Los Getters sirven para leer los valores
+     * --> Los Setters sirven para cambiar los valores */
+
+    Persona (String nombre, String primApellido) {
+        this.nombre = nombre;
+        this.primApellido = primApellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimApellido() {
+        return primApellido;
+    }
+
+    public void setPrimApellido(String primApellido) {
+        this.primApellido = primApellido;
+    }
+
+    public String getSegApellido() {
+        return segApellido;
+    }
+
+    public void setSegApellido(String segApellido) {
+        this.segApellido = segApellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void esMusico(){
+        this.nombre = nombre;
+        this.primApellido = primApellido;
+        this.segApellido = segApellido;
+        String musico;
+        if ((nombre == "Antonio") && (primApellido == "González") && (segApellido == "Flores")) {
+            musico = "Es el único músico de entre sus herma@s";
+        }else {
+            musico = "Puede que sea músico, pero no es el que cuenta la leyenda";
+        }
+        System.out.println(musico);
+    }
+}
