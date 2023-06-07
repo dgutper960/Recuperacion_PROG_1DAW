33a3e3971f0978f6520f4d9bed5a4f954280c19f
dgutper960 <dgutper960@outlook.es>
2023-06-07 11:12:16 +0200

    Inicio Act. 5_3

---
+++ src/Tema_5/Actividad_3/Main.java
@@ -0,0 +1,14 @@
+package Tema_5.Actividad_3;
+
+/** Dado un array de números enteros, escriba un programa en Java que use un objeto Map
+ * para contar el número de veces que cada número aparece en el array.
+ * El programa debe imprimir los resultados en orden ascendente según el valor del número. */
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Creamos el array con números repartidos */
+        Integer[] numeros = {2, 6, 2, 4, 5, 9, 6, 2, 1, 5, 2, 7, 7, 5, 4};
+    }
+
+}
